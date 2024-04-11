e36b800fbbfb2cf6de11ca1a81d270dbd2f2245d
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2024-04-11 01:16:29 -0300

    Columns & Rows
    Colunas e Linhas foram estudadas durante o projeto.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:project/app_controller.dart';
 
 class HomePage extends StatefulWidget {
@@ -30,8 +33,60 @@ class HomePageState extends State<HomePage> {
                   : Icon(Icons.dark_mode_outlined))),
         ],
       ),
-      body: Center(
-        child: CustomSwitch(),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Contador",
+              style: TextStyle(fontSize: 25),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Color.fromARGB(
+                      255,
+                      50 + Random().nextInt(300),
+                      50 + Random().nextInt(300),
+                      50 + Random().nextInt(300),
+                    ),
+                  ),
+                  alignment: Alignment.center,
+                  width: 50,
+                  height: 50,
+                  child: Text(
+                    "$counter",
+                    style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.pinkAccent,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
